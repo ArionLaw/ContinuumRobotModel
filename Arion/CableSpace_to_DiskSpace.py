in process of fix numerical solver

Arion/CableSpace_to_DiskSpace.py
@@ -56,23 +56,24 @@ def getCabletoDiskMapping():
 ### FK ###
 #----------------------------------------------------------------------------------------------------------------------------------------------#
 
-def DiskToCablefromLookUpTable(find):
+def DiskToCablefromLookUpTable(x):
     """
     interpolation of Cable Displacement Output from Wiper Disk Angle input from lookup table
     """
-    mask_lower = mapping['DiskAngle'].lt(find)
+    mask_lower = mapping['DiskAngle'].lt(x)
     mask_lower = mapping.loc[mask_lower]
     #print("lower: \n" , mask_lower)
-    mask_upper = mapping['DiskAngle'].gt(find)
+    mask_upper = mapping['DiskAngle'].gt(x)
     mask_upper = mapping.loc[mask_upper]
     #print("upper: \n" , mask_upper)
     y1 = mask_lower['DeltaCable'].max()
     y2 = mask_upper['DeltaCable'].min()
     x1 = mask_lower['DiskAngle'].max()
     x2 = mask_upper['DiskAngle'].min()
-    x = find
-    y = y1 + (x-x1)*(y2-y1)/(x2-x1)
-    return y
+    if np.isnan(x1):
+        return 0
+    else:
+        return (y1 + (x-x1)*(y2-y1)/(x2-x1))
 
 def DiskPosition_To_JointSpace(DiskPositions):
     roll = DiskPositions[0]/-1.56323325 #from dVRK 8mm needle driver coupling matrix
@@ -106,23 +107,25 @@ def DiskPosition_To_JointSpace(DiskPositions):
 ### IK ###
 #----------------------------------------------------------------------------------------------------------------------------------------------#
 
-def CableToDiskfromLookUpTable(find):
+def CableToDiskfromLookUpTable(x):
     """
     interpolation of Wiper Disk Angle output from Cable Displacement input from lookup table
     """
-    mask_lower = mapping['DeltaCable'].lt(find)
+    mask_lower = mapping['DeltaCable'].lt(x)
     mask_lower = mapping.loc[mask_lower]
     #print("lower: \n" , mask_lower)
-    mask_upper = mapping['DeltaCable'].gt(find)
+    mask_upper = mapping['DeltaCable'].gt(x)
     mask_upper = mapping.loc[mask_upper]
     #print("upper: \n" , mask_upper)
     y1 = mask_lower['DiskAngle'].max()
     y2 = mask_upper['DiskAngle'].min()
     x1 = mask_lower['DeltaCable'].max()
     x2 = mask_upper['DeltaCable'].min()
-    x = find
-    y = y1 + (x-x1)*(y2-y1)/(x2-x1)
-    return y
+    if np.isnan(x1):
+        return 0
+    else:
+        return (y1 + (x-x1)*(y2-y1)/(x2-x1))
+    
 
 def getDiskAngles(roll,EE_pull,deltaL1,deltaL2,deltaL3):
     """
@@ -135,15 +138,15 @@ def getDiskAngles(roll,EE_pull,deltaL1,deltaL2,deltaL3):
     
     deltaL[deltaL<=diff] = 0
     #deltaL = deltaL - diff
-    print("cables: \n", deltaL)
+    print("Cables Delta: \n", deltaL)
 
-    if (deltaL[0] > 0):
+    if (deltaL[0] >= 0):
         Disk4 = -CableToDiskfromLookUpTable(deltaL[0])
         if (deltaL[2] > 0):
             Disk3 = -CableToDiskfromLookUpTable(deltaL[2])
         else:
             Disk3 = CableToDiskfromLookUpTable(deltaL[1])
-    elif (deltaL[1] > 0):
+    elif (deltaL[1] >= 0):
         Disk3 = CableToDiskfromLookUpTable(deltaL[1])
         if (deltaL[2] > 0):
             Disk4 = CableToDiskfromLookUpTable(deltaL[2])

Arion/TaskSpace_to_JointSpace.py
@@ -3,24 +3,64 @@ from plotting import *
 import numpy as np
 import time
 
+
+#----------------------------------------------------------------------------------------------------------------------------------------------#
+#desired end effector orientation rotation matrix description
+#----------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+#angle = getAngleTwoVectors(pos_des,vec_z) #acos(dot(vec_des,vec_z)/norm(vec_z)/norm(vec_des))
+#angle_degrees = np.degrees(angle) #angle*180/pi
+#axis = getRotationAxis(pos_des, vec_z, angle)#cross(vec_des,vec_z)/norm(vec_z)/norm(vec_des)/sin(angle)
+
+def get_R_desired(EE_orientation_desired, tip_desired):
+    """
+    obtain rotation matrix of desired end effector orientation  
+    """
+    vec_x = np.array([1,0,0])
+    vec_z = np.array([0,0,1])
+
+    # azimuth about z axis
+    phi = EE_orientation_desired[1]/abs(EE_orientation_desired[1])*np.arccos(np.dot(EE_orientation_desired[:2],vec_x[:2])/np.linalg.norm(vec_x[:2])/np.linalg.norm(EE_orientation_desired[:2]))
+    phi_degrees = phi*180/np.pi
+    print("phi(azimuth): \n", round(phi_degrees,3))
+
+    # altitude about y axis
+    proj_des = [np.sqrt(EE_orientation_desired[0]**2 + EE_orientation_desired[1]**2) , EE_orientation_desired[2]]
+    proj_z = np.array([0,1])
+    theta = np.arccos(np.dot(proj_des,proj_z)/np.linalg.norm(proj_z)/np.linalg.norm(proj_des))
+    theta_degrees = theta*180/np.pi
+    print("theta(elevation): \n", round(theta_degrees,3))
+
+    # tool tip roll about z axis
+    tip_roll = tip_desired*np.pi/180
+    print("tool tip roll: \n", round(tip_desired,3))
+
+    R_desired = RotMtx('z',phi)@RotMtx('y',theta)@RotMtx('z',tip_roll)
+    print("desired Rotation matrix: \n", R_desired)
+    return R_desired
+
 #----------------------------------------------------------------------------------------------------------------------------------------------#
 ### FK ###
 #----------------------------------------------------------------------------------------------------------------------------------------------#
-def get_wristPosition_from_PSMjoints(psm_pitch,psm_yaw,psm_insertion):
+def get_wristPosition_from_PSMjoints(psm_joints):
     """
     obtain position of wrist before notches from yaw, pitch and insertion joint values
+    joint[0] = yaw
+    joint[1] = pitch
+    joint[2] = insertion
     """
-    EE_x = psm_insertion*np.sin(psm_yaw)*np.cos(psm_pitch)
-    EE_y = -psm_insertion*np.sin(psm_pitch)
-    EE_z = -psm_insertion*np.cos(psm_yaw)*np.cos(psm_pitch)
+    EE_x = psm_joints[2]*np.sin(psm_joints[0])*np.cos(psm_joints[1])
+    EE_y = -psm_joints[2]*np.sin(psm_joints[1])
+    EE_z = -psm_joints[2]*np.cos(psm_joints[0])*np.cos(psm_joints[1])
     return [EE_x,EE_y,EE_z]
 
-def get_R_shaft(psm_yaw,psm_pitch):
+def get_R_shaft(psm_joints):
     """
     calculates rotation matrix of instrument prior to roll joint
     derived from daVinci PSM modified DH convention
     """
-    R = RotMtx('x',np.pi/2)@RotMtx('z',(psm_yaw+np.pi/2))@RotMtx('x',-np.pi/2)@RotMtx('z',(psm_pitch-np.pi/2))@RotMtx('x',np.pi/2)
+    R = RotMtx('x',np.pi/2)@RotMtx('z',(psm_joints[0]+np.pi/2))@RotMtx('x',-np.pi/2)@RotMtx('z',(psm_joints[1]-np.pi/2))@RotMtx('x',np.pi/2)
     return R
 
 def get_R_fullwristmodel(roll,gamma,beta,alpha):
@@ -62,8 +102,8 @@ def IK_update(R_desired,roll,gamma,beta,alpha):
     """
     start_time = time.time()
     i=0
-    orientation_error = 1
-    previous_error = 2
+    orientation_error = 1 #arbitrary value to enter loop
+    previous_error = 2 #arbitrary value to prevent triggering exit condition(stuck in local minima)
     exit = False
     while (i<25) and (orientation_error>0.005) and exit == False:
         i=i+1
@@ -71,7 +111,7 @@ def IK_update(R_desired,roll,gamma,beta,alpha):
         orientation_error = get_O_error(R_desired,roll,gamma,beta,alpha)
         #print("orientation error: ", orientation_error)
 
-        delta = 0.25*orientation_error
+        delta = 0.23*orientation_error
         if (abs(previous_error - orientation_error)) < 0.00001:
             exit = True
         

Arion/wrist model numerical IK and FK.py
@@ -68,39 +68,6 @@ w = r*np.sin(np.radians(30))
 shaft_length = 200 #mm
 
 #----------------------------------------------------------------------------------------------------------------------------------------------#
-#desired end effector orientation rotation matrix description
-#----------------------------------------------------------------------------------------------------------------------------------------------#
-vec_x = np.array([1,0,0])
-vec_z = np.array([0,0,1])
-pos_desired = np.array([-1,1,-0.5])#np.array([1,1,0])
-tip_desired = 0 #in degrees
-
-EE_pos_desired = [0,0,-100] #[25,25,-25] #in mm
-
-#angle = getAngleTwoVectors(pos_des,vec_z) #acos(dot(vec_des,vec_z)/norm(vec_z)/norm(vec_des))
-#angle_degrees = np.degrees(angle) #angle*180/pi
-#axis = getRotationAxis(pos_des, vec_z, angle)#cross(vec_des,vec_z)/norm(vec_z)/norm(vec_des)/sin(angle)
-
-# azimuth about z axis
-phi = pos_desired[1]/abs(pos_desired[1])*np.arccos(np.dot(pos_desired[:2],vec_x[:2])/np.linalg.norm(vec_x[:2])/np.linalg.norm(pos_desired[:2]))
-phi_degrees = phi*180/np.pi
-print("phi(azimuth): \n", round(phi_degrees,3))
-
-# altitude about y axis
-proj_des = [np.sqrt(pos_desired[0]**2 + pos_desired[1]**2) , pos_desired[2]]
-proj_z = [0,1]
-theta = np.arccos(np.dot(proj_des,proj_z)/np.linalg.norm(proj_z)/np.linalg.norm(proj_des))
-theta_degrees = theta*180/np.pi
-print("theta(elevation): \n", round(theta_degrees,3))
-
-# tool tip roll about z axis
-tip_roll = tip_desired*np.pi/180
-print("tool tip roll: \n", round(tip_desired,3))
-
-R_desired = RotMtx('z',phi)@RotMtx('y',theta)@RotMtx('z',tip_roll)
-print("desired Rotation matrix: \n", R_desired)
-
-#----------------------------------------------------------------------------------------------------------------------------------------------#
 ### FK ###
 #----------------------------------------------------------------------------------------------------------------------------------------------#
 # initial joint configurations
@@ -115,9 +82,11 @@ psm_yaw = 0.7854; #0*np.pi/180;
 psm_pitch = -0.6155; #0*np.pi/180;
 psm_insertion = 43.3013; #100;
 psm_joints = [psm_yaw, psm_pitch, psm_insertion]
-print("PSM Joint Values(yaw,pitch,insertion): \n",psm_joints)
-disk_positions = [-0.2136763752838734, -1, 0.26000965425093653, 0.12707334917195753] # [roll , EE , Disk 3, Disk 4]
+
+disk_positions = [-1.5199474036199716, -1, 0.19793356053096625, 0.1984564757555433] # [roll , EE , Disk 3, Disk 4]
 joint_values = DiskPosition_To_JointSpace(disk_positions)
+
+print("PSM Joint Values(yaw,pitch,insertion): \n",psm_joints)
 print("Instrument Joint Values: \n" , joint_values)
 roll = joint_values[0]
 EE_grip = joint_values[1]
@@ -126,11 +95,11 @@ beta = joint_values[3]
 alpha = joint_values[4]
 
 # wrist position FK
-EE_pos_FK = get_wristPosition_from_PSMjoints(psm_pitch,psm_yaw,psm_insertion)
+EE_pos_FK = get_wristPosition_from_PSMjoints(psm_joints)
 print("Wrist Position: \n", EE_pos_FK)
 
 # orientation FK
-R_shaft = get_R_shaft(psm_yaw,psm_pitch)
+R_shaft = get_R_shaft(psm_joints)
 R_wrist = get_R_fullwristmodel(roll,gamma,beta,alpha)
 R_currentFK = R_shaft@R_wrist
 print("Shaft Orientation: \n", R_shaft)
@@ -140,19 +109,39 @@ print("Current EE Orientation: \n", R_currentFK)
 #----------------------------------------------------------------------------------------------------------------------------------------------#
 ### IK ###
 #----------------------------------------------------------------------------------------------------------------------------------------------#
-
 print("\n IK")
+
+# initial joint configurations
+""""""
+roll = 0*np.pi/180
+gamma = 0; #35*np.pi/180; 
+beta  = 0; #25*np.pi/180; 
+alpha = 0; #0*np.pi/180;
+
+EE_orientation_desired = np.array([0.25,0.25,-0.5])#np.array([1,1,0])
+tip_desired = 0 #in degrees
+PSM_wrist_pos_desired = [0,0,-100] #[25,25,-25] #in mm
+
 # wrist position IK
-psm_joints = get_PSMjoints_from_wristPosition(EE_pos_desired)
+psm_joints = get_PSMjoints_from_wristPosition(PSM_wrist_pos_desired)
 print("PSM Joint Values(yaw,pitch,insertion): \n", psm_joints)
+
+# shaft orientation FK for current position
+R_desired = get_R_desired(EE_orientation_desired, tip_desired)
+R_shaft = get_R_shaft(psm_joints)
+# wrist orientation FK for current position
+R_wrist = get_R_fullwristmodel(roll,gamma,beta,alpha)
+R_currentFK = R_shaft@R_wrist
+print("Shaft Orientation: \n", R_shaft)
+print("Wrist Orientation: \n", R_wrist)
+print("Current EE Orientation: \n", R_currentFK)
 
 # EE_orientation IK
 R_wrist_desired = np.linalg.inv(R_shaft)@R_desired
 print("R_desired_wrist: \n", R_wrist_desired)
 joint_angles = [roll,gamma,beta,alpha]
-print("initial notch joint angles: ",joint_angles)
 joint_angles = IK_update(R_wrist_desired,joint_angles[0],joint_angles[1],joint_angles[2],joint_angles[3])
-print("notch joint angles(roll, gamma, beta, alpha): \n", joint_angles)
+print("Notch Joint Angles(roll, gamma, beta, alpha): \n", joint_angles)
 R_wrist_IK = get_R_fullwristmodel(joint_angles[0],joint_angles[1],joint_angles[2],joint_angles[3])
 #print("R_wrist_IK: \n", R_wrist_IK)
 #print("R_desired_wrist: \n", R_wrist_desired)